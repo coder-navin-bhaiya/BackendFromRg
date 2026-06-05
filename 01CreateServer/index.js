3d3cb98382ae8221fe1270d5ed3dc160b3d53e13
Navin Kumar <navin6824@gmail.com>
2026-06-05 16:54:25 +0530

    Express server

--- 01CreateServer/index.js
+++ 01CreateServer/index.js
@@ -1,11 +1,31 @@
 // step-1)
  const http = require('http');
 
-
  //step-2) creating server
  const server = http.createServer((req,res) =>{
-    res.end("First response send by server")
+
+    //  res.end("First response send by server")
+    
+
+
+    //Using routing
+     if(req.url === '/'){
+        res.end("First response send by server")
+    }
+    else if(req.url === '/Contact'){
+        res.end("It is contact routing");
+    }
+
+    else if(req.url === '/About'){
+        res.end("It is About routing");
+    }
+    else{
+        res.end("Error:Page not found");
+    }
+
+
  });
+ 
 //  console.log(server); o/p = object
 
 //step-3) listen krna hai
